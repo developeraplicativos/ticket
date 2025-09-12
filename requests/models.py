489c0c6510e4963aeb9191b2ec2170be90d181b0
developer.aplicativos <developer.aplicativos@gmail.com>
2025-09-11 23:21:10 -0300

    read e tests feitos

--- requests/models.py
+++ requests/models.py
@@ -16,27 +16,42 @@ class Requests(models.Model):
             self.about = about
             self.title = title
             self.save()
+            return True
 
         except Exception as Ex:
             print( Ex )
+            return False
+ 
+    def readRequest(self, id):
+        try: 
+            if id is None or isinstance(id, float): 
+                raise ValueError('É preciso que exista um valor numérico') 
+            return self.__class__.objects.filter(pk=id).first()  # retorna None se não achar
+
+        except Exception as Ex:
+            print( Ex )
+            return None
 
 class RequestsCustomer(Requests): 
     def addRequest(self, about, title):
         '''the client may not know the title for some situation'''
         try: 
-            if(title is None): 
-                title = 'Quero resolver algo'
+            if(title is None or title == ''): 
+                title = 'Quero resolver algo: '
             if(about is None): 
                 raise ValueError('O autor não pode ser vazio')
             self.about = about
             self.title = title
             self.save()
+            return True
 
         except Exception as Ex:
             print( Ex )
+            return False
 
+    def readRequest(self, id): 
+        return super().readRequest(id)
     
 
 class RequestsInternal(Requests): 
-    def addRequest(self, about, title): 
-        super().addRequest(about, title) 
+    pass 
--- requests/tests.py
+++ requests/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from .models import Requests,RequestsCustomer,RequestsInternal
 
-# Create your tests here.
+class TestClass(TestCase): 
+    def test_save_RequestsCustomer(self):
+        req = RequestsCustomer()
+        result = req.addRequest("Preciso de ajuda", None)
+        self.assertTrue(result)
+
+    def test_read_RequestsCustomer(self):
+        req = RequestsCustomer()
+        req.addRequest("Preciso de ajuda", None)  
+        found = req.readRequest(req.id)  
+        self.assertIsNotNone(found) 
+
+
+    def test_save_RequestsInternal(self):
+        tst = RequestsInternal()
+        tst.addRequest("Preciso de ajuda", 'teste bem sussedido')
+        self.assertEqual(tst.title, 'teste bem sussedido')
+
+ 
+    def test_read_RequestsInternal(self):
+        req = RequestsInternal()
+        req.addRequest("Preciso de ajuda", 'Opa')  
+        found = req.readRequest(req.id)  
+        self.assertIsNotNone(found) 
